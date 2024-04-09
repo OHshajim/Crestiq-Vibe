dbc1d622e14553d9cdc4aa7dd72f7b21e60747e6
OHshajim <ajshajimmax@gmail.com>
2024-04-09 23:59:01 +0600

    user login and register data save

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,29 +1,44 @@
 import { Link } from "react-router-dom";
+import Navbar from "../../Components/Nav/Navbar";
+import { useContext } from "react";
+import { AuthContext } from "../../Provider/AuthProvider";
 
 const Register = () => {
-    return (
+    const { register } = useContext(AuthContext)
+    const handleRegister = e => {
+        e.preventDefault()
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
+        // log in 
+        register(email, password)
+            .then(result => console.log(result.user))
+            .catch(error => console.log(error))
+    }
+
+return (
+    <div>
         <div>
+            <Navbar />
+
             <div className="flex flex-col justify-center items-center w-screen h-[80vh]">
-                <form className=" max-w-[1200px] w-full space-y-5">
-                    <label className="input input-bordered flex items-center gap-2">
+                <form className=" max-w-[700px] w-full bg-[#050a15] py-16 px-10 rounded-lg" onSubmit={handleRegister}>
+                    <h3 className="text-3xl font-bold">Welcome Back </h3>
+                    <p className="mt-2">Already have an Account ? <Link to="/login" className=" btn-link link-hover" >Log in</Link></p>
+                    <label className="input input-bordered flex items-center gap-2 bg-black my-5">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
-                        <input type="text" className="grow" placeholder="Email" />
-                    </label>
-                    <label className="input input-bordered flex items-center gap-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
-                        <input type="text" className="grow" placeholder="Username" />
+                        <input type="email" className="grow" placeholder="Email" name="email" required />
                     </label>
 
-                    <label className="input input-bordered flex items-center gap-2">
+                    <label className="input input-bordered flex items-center gap-2 bg-black">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clipRule="evenodd" /></svg>
-                        <input type="password" className="grow" placeholder="Password" />
+                        <input type="password" className="grow" placeholder="password" name="password" required />
                     </label>
-                    <input type="submit" value="Register" className="w-full btn " />
+                    <input type="submit" value="Submit" className="w-full btn hover:bg-black mt-5" />
                 </form>
-                <p className="mt-2">Already have an Account ? <Link to="/login" className=" btn-link link-hover" >Log in</Link></p>
             </div>
         </div>
-    );
-};
-
+    </div>
+);
+}
 export default Register;
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,13 +1,44 @@
-import { createContext, useState } from 'react';
-const AuthContext = createContext(AuthProvider)
-const AuthProvider = ({ Children }) => {
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
+import { createContext, useEffect, useState } from 'react';
+import auth from '../Firebase/Firebase.config';
+import PropTypes from 'prop-types'; // ES6
+
+
+export const AuthContext = createContext(null)
+
+const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null)
-    const authInfo = { user }
+
+
+    // Sign Up
+    const register = (email, password) => {
+        // setLoad(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    };
+
+    // Login
+    const login = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
+
+    }
+    useEffect(() => {
+        const currentUser = onAuthStateChanged(auth, (user) => {
+            setUser(user)
+        });
+        return () => currentUser()
+    }, [])
+
+
+    const authInfo = { user, login, register }
     return (
         <AuthContext.Provider value={authInfo}>
-            {Children}
+            {children}
         </AuthContext.Provider>
     );
 };
+
+AuthProvider.propTypes = {
+    children: PropTypes.node,
+}
 
 export default AuthProvider;
