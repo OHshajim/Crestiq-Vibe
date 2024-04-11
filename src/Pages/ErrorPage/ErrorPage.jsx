c11c108cf2efc4a991c882ceee998713e7191710
OHshajim <ajshajimmax@gmail.com>
2024-04-11 23:50:45 +0600

    error page update

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -2,15 +2,15 @@ import { Link, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
     const error = useRouteError()
-    console.log(error.status);
+    console.log(error);
     return (
         <div className="h-screen w-screen flex flex-col items-center justify-center">
-            <h3 className='text-xl'>Opssss!!! </h3>
-            <p>{error.status }</p>
+            <img src="https://i.ibb.co/xzRYDgn/3747371.jpg" alt="Error" className="max-w-[1600px] h-[80vh]"/>
+            <p className="text-red-600 font-bold my-5">{error.error.message }</p>
             {
                 error.status === 404 && <div className=" flex flex-col items-center">
                     <h3>This page is not found</h3>
-                    <Link to={'/'}><button className="btn bg-[tomato] my-4 text-black"> Back to Home</button></Link>
+                    <Link to={'/'}><button className="btn bg-[tomato] my-2 text-black"> Back to Home</button></Link>
                 </div>
             }
         </div>
