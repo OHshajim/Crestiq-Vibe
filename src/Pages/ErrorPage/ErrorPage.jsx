458f7ea73e8b9df34e41e2b855cf56e6ad460c02
OHshajim <ajshajimmax@gmail.com>
2024-04-11 23:54:49 +0600

    error page more  update

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -4,13 +4,13 @@ const ErrorPage = () => {
     const error = useRouteError()
     console.log(error);
     return (
-        <div className="h-screen w-screen flex flex-col items-center justify-center">
-            <img src="https://i.ibb.co/xzRYDgn/3747371.jpg" alt="Error" className="max-w-[1600px] h-[80vh]"/>
-            <p className="text-red-600 font-bold my-5">{error.error.message }</p>
+        <div className=" flex flex-col items-center justify-center">
+            <img src="https://i.ibb.co/xzRYDgn/3747371.jpg" alt="Error" className=""/>
+            <p className="text-red-600 font-bold mt-5">{error.error.message }</p>
             {
                 error.status === 404 && <div className=" flex flex-col items-center">
                     <h3>This page is not found</h3>
-                    <Link to={'/'}><button className="btn bg-[tomato] my-2 text-black"> Back to Home</button></Link>
+                    <Link to={'/'}><button className="btn bg-[tomato] my-2 text-white font-bold text-lg hover:bg-black"> Back to Home</button></Link>
                 </div>
             }
         </div>
