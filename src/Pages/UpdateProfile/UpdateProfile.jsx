81c060b811e8d319d7a99d0bdd17d56c33cabad3
OHshajim <ajshajimmax@gmail.com>
2024-04-14 03:33:28 +0600

    update profile fixed

--- src/Pages/UpdateProfile/UpdateProfile.jsx
+++ src/Pages/UpdateProfile/UpdateProfile.jsx
@@ -1,18 +1,32 @@
 import { useContext } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
 import Navbar from "../../Components/Nav/Navbar";
-import { useForm } from "react-hook-form";
 import { Helmet } from "react-helmet-async";
+import { updateProfile } from "firebase/auth";
+import auth from "../../Firebase/Firebase.config";
 
 const UpdateProfile = () => {
     const { user, isLoading } = useContext(AuthContext)
     console.log(user);
     const { displayName, email, photoURL } = user
-    const { register, handleSubmit } = useForm();
-    const onSubmit = data => {
-        console.log(data);
+    const handleUpdate = e => {
+        e.preventDefault()
+        const email = e.target.email.value;
+        const name = e.target.name.value;
+        const image = e.target.image.value;
+        console.log(email, name, image);
 
+        updateProfile(auth.currentUser, {
+            displayName: name, photoURL: image
+        })
+            .then(() => {
+                console.log(auth.currentUser)
+            })
+            .catch((error) => {
+                console.log(error)
+            });
     }
+
     return (
         <div className="max-w-[1600px] flex flex-col  mx-auto h-[90vh]">
             <Helmet><title>Crestiq Vibe || Update Profile</title></Helmet>
@@ -29,14 +43,14 @@ const UpdateProfile = () => {
                         <img src={photoURL} />
                     </div>
                 </div>
-                <form className="form-control max-w-xl w-full " onSubmit={handleSubmit(onSubmit)}>
+                <form className="form-control max-w-xl w-full " onSubmit={handleUpdate}>
 
                     <label className="">Name</label>
-                    <input {...register("name")} name="name" type="text" defaultValue={displayName} className="px-3 py-2 " />
+                    <input name="name" type="text" defaultValue={displayName} className="px-3 py-2 " required/>
                     <label >Email</label>
-                    <input type="email" defaultValue={email} className="px-3 py-2 " />
+                    <input type="email" value={email} name="email" className="px-3 py-2 " />
                     <label >Photo URL</label>
-                    <input type="text" defaultValue={photoURL} className="px-3 py-2 " />
+                    <input type="text" defaultValue={photoURL} name="image" className="px-3 py-2 " required/>
                     <input type="submit" value="Update" className="btn " />
                 </form>
             </div>
