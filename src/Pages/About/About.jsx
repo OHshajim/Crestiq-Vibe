9c1313643d94cfb346cdaf66e2440c8723981b9a
OHshajim <ajshajimmax@gmail.com>
2024-04-17 07:24:14 +0600

    bugs fixed

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet-async";
 import Achievement from "../../Components/Achievement/Achievement";
 import Contact from "../../Components/Contact/Contact";
 import Team from "../../Components/Team/Team";
@@ -7,17 +8,17 @@ const About = () => {
 
     return (
         <div>
-
+            <Helmet><title>Crestiq Vibe || About Us</title></Helmet>
             <div className="py-20">
                 <h1 className="text-center text-2xl lg:text-4xl my-3">Discover the Crestiq Vibe Difference: Our Story</h1>
                 <img src="https://demo.xstheme.com/lacasa/wp-content/uploads/sites/13/2018/01/lamsum.jpg"
-                 alt="" className="w-full"/>
+                    alt="" className="w-full" />
             </div>
 
             <div className="max-w-[1600px] mx-auto">
-                <Team/>
-                <Achievement/>
-                <Contact/>
+                <Team />
+                <Achievement />
+                <Contact />
             </div>
         </div>
     );
--- src/Pages/EstateDetails/EstateDetails.jsx
+++ src/Pages/EstateDetails/EstateDetails.jsx
@@ -1,6 +1,7 @@
 import { useLoaderData, useParams } from "react-router-dom";
 import Navbar from "../../Components/Nav/Navbar";
 import { IoLocationOutline } from "react-icons/io5";
+import { Helmet } from "react-helmet-async";
 
 const EstateDetails = () => {
     const { properties } = useLoaderData()
@@ -9,13 +10,14 @@ const EstateDetails = () => {
     // console.log(id.id, property);
     const { view_property_url, estate_title, segment_name, description, price, status, area, location, facilities } = property
     return (
-        <div className="max-w-[1700px] mx-auto">
-            <Navbar />
+        <div className="max-w-[1600px] mx-auto">
+                        <Helmet><title>Crestiq Vibe || Property Details</title></Helmet>
+            <h1 className="text-5xl  font-bold px-4  sm:px-10  pt-28">Property Details </h1>
             {/* {
-                isLoading && <div className="flex justify-center items-center my-20"><span className="w-20 loading loading-spinner text-error"></span></div>
+                isLoading && <div className="flex justify-center items-center py-20"><span className="w-20 loading loading-spinner text-error"></span></div>
             } */}
-            <div className=" flex flex-col lg:flex-row items-center justify-center  p-10  bg-base-100 shadow-xl mb-20 ">
-                <figure><img src={view_property_url} alt="view property url" className=" max-w-2xl" /></figure>
+            <div className=" flex flex-col lg:flex-row items-center justify-center p-4  sm:p-10 bg-base-100 shadow-xl mb-20 ">
+                <figure><img src={view_property_url} alt="view property url" className=" max-w-2xl lg:w-fit w-full" /></figure>
                 <div className=" lg:px-10 lg:py-0  py-10 ">
                     <h2 className="text-3xl font-bold ">{estate_title}</h2>
                     <h3 className="text-lg font-medium ">{segment_name}</h3>
